0a1b88b81b5a62498ce2ffec052c41d92d04fc69
Владимир Ветровd:/Dev/backend_test_homework/venv/Scripts/python.exe <vsvetrov@gmail.com>
2022-08-26 10:00:57 +0300

    v9

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -112,7 +112,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
     ),
-    'DEFAULT_PAGINATION_CLASS': 'api.pagination.CustomPageNumberPagination',
 }
 
 DJOSER = {
