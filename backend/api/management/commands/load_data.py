cfe670c5364ad3543d6b23499146474ae246331e
Владимир Ветровd:/Dev/backend_test_homework/venv/Scripts/python.exe <vsvetrov@gmail.com>
2022-09-01 06:20:00 +0300

    v10

--- backend/api/management/commands/load_data.py
+++ backend/api/management/commands/load_data.py
@@ -1,8 +1,6 @@
 import json
-
-from django.core.management.base import BaseCommand
-
 from api.models import Ingredient
+from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
